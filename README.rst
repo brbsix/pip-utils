faf633ba73997fb2ebea5ace68d715c9f4205dd5
Six <brbsix@gmail.com>
2016-04-25 14:00:08 -0700

    Initial commit of packaging-related files

---
+++ README.rst
@@ -0,0 +1,144 @@
+pip-utils
+---------
+
+Helper utilities for pip.
+
+
+Installation
+============
+
+::
+
+    pip install --user pip-utils
+
+
+Usage
+=====
+
+::
+
+    usage: pip3-utils [-h] [--version]
+                      {dependants,dependents,locate,outdated,parents} ...
+
+    Helper utilities for pip.
+
+    positional arguments:
+      {dependants,dependents,locate,outdated,parents}
+        dependants          list dependants of package
+        dependents          list dependents of package
+        locate              identify package that file belongs to
+        outdated            list outdated packages that may be updated
+        parents             list packages lacking dependants
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --version             show program's version number and exit
+
+    See `pip3-utils COMMAND --help` for help on a specific subcommand.
+
+For Python 2, use ``pip2-utils``. For Python 3, use ``pip3-utils``. Or, use ``python -m pip_utils``.
+
+
+Commands
+========
+
+List dependants of a package:
+
+::
+
+    $ pip3-utils dependants pexpect
+    ipython
+
+List direct dependents of a package:
+
+::
+
+    $ pip3-utils dependents ipython
+    backports.shutil-get-terminal-size
+    decorator
+    pexpect
+    pickleshare
+    setuptools
+    simplegeneric
+    traitlets
+
+List direct dependents of a package with their version requirements:
+
+::
+
+    $ pip3-utils dependents -i dataset
+    alembic>=0.6.2
+    normality>=0.2.2
+    PyYAML>=3.10
+    six
+    six>=1.7.3
+    sqlalchemy>=0.9.1
+
+List all dependencies of a package recursively:
+
+::
+
+    $ pip3-utils dependents -r dataset
+    alembic
+    Mako
+    MarkupSafe
+    normality
+    python-editor
+    PyYAML
+    six
+    SQLAlchemy
+
+Identify package(s) that file belongs to:
+
+::
+
+    $ pip3-utils locate ~/.local/bin/symilar
+    pylint
+
+List outdated packages that may be updated. Note, this differ's from pip's ``--outdated`` flag in that it verifies that there are no conflicting dependencies that would otherwise make an update inadvisable.
+
+::
+
+    pip3-utils outdated
+
+List all outdated packages. Note, this is equivalent to pip's ``--outdated`` flag.
+
+::
+
+    pip3-utils outdated --all
+
+List outdated packages unable to be updated due to dependency requirements:
+
+::
+
+    pip3-utils outdated --pinned
+
+List packages lacking dependants:
+
+::
+
+    pip3-utils parents
+
+
+Development
+===========
+
+To run tests (automatically pulling in dependencies):
+
+::
+
+    python setup.py test
+
+To run tests directly, with verbose output:
+
+::
+
+    python3 -m pytest -vv
+
+
+License
+=======
+
+Copyright (c) 2015 Six (brbsix@gmail.com).
+
+Licensed under the GPLv3 license.
---
+++ setup.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Application setup script
+
+To build package:
+python3 setup.py sdist bdist_wheel clean
+
+To run tests in an virtualenv:
+python3 setup.py test
+
+To run tests directly with verbose output:
+python3 -m pytest -vv
+"""
+
+# Python 2 forwards-compatibility
+from __future__ import absolute_import
+
+# standard imports
+import io
+import os
+import re
+import sys
+
+# external imports
+from setuptools import setup
+
+# application imports
+from pip_utils import __description__, __program__, __version__
+
+
+def long_description():
+    """Return the contents of README.rst (with badging removed)."""
+    # use re.compile() for flags support in Python 2.6
+    pattern = re.compile('^.. start-badges.*^.. end-badges',
+                         flags=re.M | re.S)
+    return pattern.sub('', read('README.rst'))
+
+
+def read(*names, **kwargs):
+    """Return contents of text file (in the same directory as this file)."""
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+SETUP_REQUIRES = ['pytest-runner']
+INSTALL_REQUIRES = ['pip']
+TESTS_REQUIRE = []
+
+# pylint will only install on Python 2.7.x and later
+if sys.version_info >= (2, 7):
+    TESTS_REQUIRE.append('pytest-pylint')
+    os.environ['PYTEST_ADDOPTS'] = '--pylint'
+
+setup(
+    name=__program__,
+    version=__version__,
+    description=__description__,
+    author='Brian Beffa',
+    author_email='brbsix@gmail.com',
+    long_description=long_description(),
+    url='https://github.com/brbsix/pip-utils',
+    license='GPLv3',
+    keywords=['package', 'pip', 'PyPi'],
+    packages=['pip_utils'],
+    install_requires=INSTALL_REQUIRES,
+    setup_requires=SETUP_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'testing': TESTS_REQUIRE},
+    entry_points={
+        'console_scripts': [
+            'pip{v}-utils=pip_utils.cli:main'.format(
+                v=sys.version_info[0])  # not a named tuple in Python 2.6
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: System',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+    ]
+)

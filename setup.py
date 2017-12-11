837133574ebaab0e0125ce26d1cb4fa928f637d8
Six <brbsix@gmail.com>
2017-12-11 05:04:28 -0800

    Update classifiers

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System',
         'Topic :: System :: Software Distribution',
         'Topic :: System :: Systems Administration',
